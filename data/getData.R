e3a66a06e8b41940f1d2bc8737e16eb9386a5d2c
Aaron Oustrich <aaron.oustrich@gmail.com>
2024-03-14 15:58:54 -0600

    Setup Repo

---
+++ data/getData.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+crimes <- read_csv("https://richardson.byu.edu/data_contest/2024/Crimes.csv")
+weather <- read_csv("https://richardson.byu.edu/data_contest/2024/weather.csv")
+holidays <- read_csv("https://richardson.byu.edu/data_contest/2024/holidays.csv")
+fullMoons <- read_csv("https://richardson.byu.edu/data_contest/2024/full_moon.csv")
